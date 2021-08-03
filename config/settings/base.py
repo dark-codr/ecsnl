19b3f8f30717399ea388712e63918f3a0b58bbd8
jeremiah david <jeremiahedavids@gmail.com>
2021-08-03 07:55:50 +0100

    resolved duplicate sites in installed apps

--- config/settings/base.py
+++ config/settings/base.py
@@ -85,7 +85,6 @@ DJANGO_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.sites",
-    "django.contrib.sites",
     "django.contrib.sitemaps",
     "django.contrib.messages",
     "django.contrib.staticfiles",
